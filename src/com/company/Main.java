f05ca5ccfd6e8e0e8b5948ce1d932e76386083a6
TBlais10 <blaislighting@gmail.com>
2021-08-18 21:37:37 -0400

    First commit. Main contains thoughts on approaching this project.

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+/*The following classes and methods will be needed:
+* Main class:
+* will be calling the run methods in each car class
+*
+* CLI class:
+* add some fail safe scanners that is careful about what the user puts into the program.
+*
+* Menu Class:
+* should contain a submenu method which will ask the user wants to return to the main menu or not.
+*
+* CarRentalService Class:
+* will contain atleast one method that will be called in the main.
+*
+* Cars class:
+* Listed inside a string array
+* A rented cars list?
+* any and all information about the cars
+*
+* Rental class(?)
+* does the math in changing the results of the program.
+* Must also keep track of which cars have been rented or not. Moving the cars between lists.
+*
+* Loops will be needed in order to cycle thru as the user rents out the cars. The loop needs to run until there are no cars.
+*
+* */
