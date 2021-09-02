9cb1cbd3e4a01313165aa508c690daa3d7871329
TBlais10 <blaislighting@gmail.com>
2021-09-01 23:08:15 -0400

    Attempted to refactor code from menu into other classes. Proved more complicated than expected. Restarted from where I began w/ previous commits. Will research further and return to project at a future date.

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,11 +1,19 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Math {
+    private final Menu menu;
 
-    DataBase inv = new DataBase();
-    ArrayList<Car> arrList = inv.getCarsAval();
-    CLI userInput = new CLI();
+    public Math(Menu menu) {
+        this.menu = menu;
+    }
 
-}
+    public void rentedTotal(int i) {
+        System.out.println(i);
+        System.out.println("\n---\nThank you for choosing the " + menu.getArrList().get(i).getDetails() + "! The price per day is " + menu.getArrList().get(i).getPrice() + ". How many days do you wish to rent it?\n---");
+        int input = menu.getUserInput().getInt();
+        int total = input * menu.getArrList().get(i).getPrice();
+        System.out.println("\n---\n For a " + input + " day rental the " + menu.getArrList().get(i).getDetails() + " will cost a total of $" + total + "! Safe Travels!\n---");
+        menu.getArrList().get(i).setTotalCost(total);
+
+    } //a quick program that will give the user the total of the rental per days they wish to rent for.
+ }
