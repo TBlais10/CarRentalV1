1b0cfe873b7c3dad1c5e8481223f3b49b62bfe52
TBlais10 <blaislighting@gmail.com>
2021-09-28 21:41:09 -0400

    Math class commented out for the moment as testing and tweaking of the idea of moving methods to new classes continues. CLI updated w/ the work that has been slowly added onto in the Dice Wars project.

--- src/com/company/CLI.java
+++ src/com/company/CLI.java
@@ -1,86 +1,161 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CLI {
 
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
-    public int getInt(int min, int max) {
-        try {
-            //String[] blarg = {}; //This is a test for error messages
-            System.out.print("Input: ");
-            int input = scanner.nextInt();
-            //String asdf = blarg[1]; //This is a test for error messages
-            if (input > max || input < min) {
-                System.out.println("Input is out of range! Please try again with a number between " + min + " and " + max + ".");
-                return getInt(min, max);
+    public static int getInt(int min, int max) {
+        int input;
+        while (true) {
+            try {
+                //String[] blarg = {}; //This is a test for error messages
+                System.out.print("Input: ");
+                input = scanner.nextInt();
+                //String asdf = blarg[1]; //This is a test for error messages
+                if (input > max || input < min) {
+                    System.out.println("Input is out of range! Please try again with a number between " + min + " and " + max + ".");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException exception) {//testing for data types outside of int
+                System.out.println("Incorrect input! Please provide a number and try again.");
+            } catch (Exception exception) {//testing for unknown errors
+                System.out.println("An unknown error appeared.");
+
             }
-            return input;
-        } catch (InputMismatchException exception) {//testing for data types outside of int
-            System.out.println("Incorrect input! Please provide a number and try again.");
-            scanner.nextLine();
-            return getInt(min, max);
-        } catch (Exception exception) {//testing for unknown errors
-            System.out.println("An unknown error appeared.");
-            scanner.nextLine();
-            return getInt(min, max);
         }
-    }
-
-    public int getInt() {
-        System.out.print("Input: ");
-        int input = scanner.nextInt();
         return input;
     }
 
-    public void exit() {
-        System.out.println("\n---\nAlrighty! Exiting the program. Please come again!\n---");
+
+    public static void exit() {
+        System.out.println("\n---\nClosing the program. See you soon!\n---");
         System.exit(0);
     }
 
-    public String getString(int min, int max) {
-        try {
-            System.out.print("Input: ");
-            String userInput = scanner.nextLine().trim();
-
-            if (userStringCheck(min, max, userInput)) return getString(min, max);
-            return userInput;
-
-        } catch (InputMismatchException exception) {
-            System.out.println("Incorrect input! Please provide a word or phrase and try again.");
-            scanner.nextLine();
-            return getString(min, max);
-        } catch (Exception exception) {
-            System.out.println("An unknown error appeared.");
-            scanner.nextLine();
-            return getString(min, max);
+    public static void exit(String statement) {
+        System.out.println("Are you sure you want to exit " + statement + " ? Y/N");
+        String answer = getString(1, 3);
+        if (answer.equalsIgnoreCase("Y")) {
+            System.out.println("Closing program! See you soon!");
+            System.exit(0);
+        } else if (answer.equalsIgnoreCase("N")) { // this statement will be dependent on the scope of the program.
+            System.out.println("Ok, returning to main menu!");
         }
     }
 
-    public String getString() {
-        System.out.print("Input: ");
-        String input = scanner.nextLine().trim();
-        if (input.length() == 0) {
-            System.out.println("The text box cannot be empty! Please try again.");
-            return getString();
+    public static String getString(int min, int max) {
+        String userInput;
+        while (true) {
+            try {
+                System.out.print("Input: ");
+                userInput = scanner.nextLine().trim();
+
+                if (Objects.equals(userInput, " ")) {
+                    System.out.println("Your input cannot be empty! Please try again.");
+                } else if (userInput.length() < min) {
+                    System.out.println("You cannot have less than " + min + " characters! Please try again");
+                } else if (userInput.length() > max) {
+                    System.out.println("You have exceeded the character limit of " + max + " by " + (userInput.length() - max) + "! Please try again.");
+                } else {
+                    break;
+                }
+
+            } catch (InputMismatchException exception) {
+                System.out.println("Incorrect input! Please provide a word or phrase and try again.");
+                scanner.nextLine();
+            } catch (Exception exception) {
+                System.out.println("An unknown error appeared.");
+                scanner.nextLine();
+            }
+        }
+        return userInput;
+    }
+
+    public static float getFloat(float min, float max){
+        float input = 0;
+        try{
+            while(true){
+                System.out.print("Input: ");
+                input = scanner.nextFloat();
+                if (max < input || min > input){
+                    System.out.println("Input out of range! Please provide a number between " + min + " and " + max + ".");
+                } else {
+                    break;
+                }
+            }
+        } catch (InputMismatchException exception){
+            System.out.println("Incorrect input! Please try again.");
+        }
+        catch (Exception exception){
+            System.out.println("Unknown error! Please try again.");
         }
         return input;
     }
 
-    private boolean userStringCheck(int min, int max, String userInput) {
-        if (userInput == " ") {System.out.println("Your input cannot be empty! Please try again.");
-            return true;
+    public static byte getByte(byte min, byte max){
+        byte input = 0;
+        try{
+            while(true){
+                input = scanner.nextByte();
+                if (input > max || input < min){
+                    System.out.println("Input out of range! Please enter a different number.");
+                } else {
+                    break;
+                }
+            }
+        } catch (InputMismatchException exception){
+            System.out.println("Incorrect input! Please try again.");
         }
-        else if (userInput.length() < min){
-            System.out.println("You cannot have less than " + min + " characters! Please try again");
-            return true;
+        catch (Exception exception){
+            System.out.println("Unknown error! Please try again.");
         }
-        else if (userInput.length() > max) {
-            System.out.println("You have exceeded the character limit of " + max + " by " + (userInput.length() - max) + "! Please try again.");
-            return true;
-        }
-        return false;
+        return input;
     }
+
+    public static String getString() {
+        String input;
+        while (true){
+            try {
+                System.out.print("Input: ");
+                input = scanner.nextLine().trim();
+
+                if (input.length() == 0) {
+                    System.out.println("The text box cannot be empty! Please try again.");
+                } else {
+                    break;
+                }
+            } catch (Exception exception){
+                System.out.println("An unknown error appeared. Please try again.");
+            }
+        }
+        return input;
+    }
+
+    public static void flavorText(String flavor, int repeat, String statement) {
+        System.out.println(flavor.repeat(repeat));
+        System.out.println(statement);
+        System.out.print(flavor.repeat(repeat));
+        System.out.println("\n");
+    }
+
+    public static void flavorText(String flavor, int repeat, ArrayList<String> statement) {
+        int randomQuote = (int) (java.lang.Math.random() * statement.size());
+
+        System.out.println(flavor.repeat(repeat));
+        System.out.println("\n");
+        System.out.println(statement.get(randomQuote));
+        System.out.println("\n");
+        System.out.print(flavor.repeat(repeat) + "\n");
+    }
+
+    public static void newTerminalScreen(){
+        System.out.println("\n".repeat(30));
+    }
+
 }
--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -8,12 +8,12 @@ public class Math {
     }
 
     public void rentedTotal(int i) {
-        System.out.println(i);
-        System.out.println("\n---\nThank you for choosing the " + menu.getArrList().get(i).getDetails() + "! The price per day is " + menu.getArrList().get(i).getPrice() + ". How many days do you wish to rent it?\n---");
-        int input = menu.getUserInput().getInt();
-        int total = input * menu.getArrList().get(i).getPrice();
-        System.out.println("\n---\n For a " + input + " day rental the " + menu.getArrList().get(i).getDetails() + " will cost a total of $" + total + "! Safe Travels!\n---");
-        menu.getArrList().get(i).setTotalCost(total);
+//        System.out.println(i);
+//        System.out.println("\n---\nThank you for choosing the " + menu.getArrList().get(i).getDetails() + "! The price per day is " + menu.getArrList().get(i).getPrice() + ". How many days do you wish to rent it?\n---");
+//        int input = menu.getUserInput().getInt();
+//        int total = input * menu.getArrList().get(i).getPrice();
+//        System.out.println("\n---\n For a " + input + " day rental the " + menu.getArrList().get(i).getDetails() + " will cost a total of $" + total + "! Safe Travels!\n---");
+//        menu.getArrList().get(i).setTotalCost(total);
 
     } //a quick program that will give the user the total of the rental per days they wish to rent for.
  }
